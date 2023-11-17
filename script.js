4476e13ebb7d17c569b502d20b49eab3dd84ef5d
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-11-17 21:05:55 +0530

    working on next button

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ for(let i=0;i<questions.length;i++)
     {
         questions[i].style.display="none";
     }
-
-   
 }
 if(currentQuestionNo==3)
 {
@@ -31,7 +29,8 @@ function showScore()
     div.classList.add("score");
     container.appendChild(div);
 }
-
+mainFun();
+function mainFun(){
 container.addEventListener("click",function(e){
     if(e.target.tagName==="INPUT")
     {
@@ -41,7 +40,8 @@ container.addEventListener("click",function(e){
         e.target.style.backgroundColor="rgb(138, 138, 241)";
         let ol=e.target.parentElement.parentElement;
         let inputArray=ol.querySelectorAll("ol > li > input");
-        for(let i=0;i<inputArray.length;i++)
+        console.log(inputArray);
+        for(let i=0;i<i4;i++)
         {
             inputArray[i].disabled=true;
         }
@@ -55,38 +55,44 @@ container.addEventListener("click",function(e){
         e.target.style.backgroundColor="rgb(206, 51, 51)";
         let ol=e.target.parentElement.parentElement;
         let inputArray=ol.querySelectorAll("ol > li > input");
-        for(let i=0;i<inputArray.length;i++)
+        console.log(inputArray);
+        for(let i=0;i<4;i++)
         {
             inputArray[i].disabled=true;
         }
-        // coloringRightAnswer();
         for(let i=0;i<inputArray.length;i++)
         {
             if(inputArray[i].getAttribute("value")==ans)
             {
-                inputArray[i].style.backgroundColor="blue";
+                inputArray[i].style.backgroundColor="rgb(138, 138, 241)";
             }
         }
         addNextButton();
       }
     }
-    currentQuestionNo++;
-    // visibleQuestion();
-    
+   
+    clickNext();
   });
+}
 
+let nextBtn;
 function addNextButton()
 {
-    let nextBtn=document.createElement("input");
+     nextBtn=document.createElement("input");
     nextBtn.innerHTML="Next";
+    nextBtn.type="button";
     nextBtn.value="NEXT";
     nextBtn.classList.add("nextButton");
     qst.appendChild(nextBtn);
 }
 
 
-// function clickNext(){
-//     nextBtn.addEventListener("click",function(e){ 
-//         visibleQuestion();
-//     });
-// }
+function clickNext(){
+    nextBtn.addEventListener("click",function(){ 
+        nextBtn.disabled=true;
+        visibleQuestion();
+        // addNextButton();
+        // mainFun();
+    });
+}
+currentQuestionNo++;
