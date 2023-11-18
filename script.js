5a01f2aab1580bbabdea66a095a5af0f31fe00bc
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-11-18 14:14:20 +0530

    3 questions added (with the score) successfully

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let container=document.querySelector(".container");
 let questions=document.getElementsByClassName("question");
-let qst=document.getElementById("question");
+let btn=document.getElementsByTagName("button");
 let correctAns=0;
+let currentQuestion;
 let currentQuestionNo=0;
 visibleQuestion();
 function visibleQuestion()
 {
 for(let i=0;i<questions.length;i++)
 {
-    if(i==currentQuestionNo )
+    if(i==currentQuestionNo)
     {
         questions[i].style.display="block";
+        currentQuestion=questions[i];
     }
     else
     {
@@ -21,6 +23,7 @@ if(currentQuestionNo==3)
 {
     showScore();
 }
+mainFun();
 }
 function showScore()
 {
@@ -29,37 +32,35 @@ function showScore()
     div.classList.add("score");
     container.appendChild(div);
 }
-mainFun();
+
 function mainFun(){
-container.addEventListener("click",function(e){
+    currentQuestion.addEventListener("click",function(e){
     if(e.target.tagName==="INPUT")
     {
     let ans= e.target.parentElement.parentElement.getAttribute("ans");
       if(e.target.value==ans)
       {
+        correctAns+=1;
         e.target.style.backgroundColor="rgb(138, 138, 241)";
         let ol=e.target.parentElement.parentElement;
         let inputArray=ol.querySelectorAll("ol > li > input");
-        console.log(inputArray);
-        for(let i=0;i<i4;i++)
+        for(let i=0;i<4;i++)
         {
             inputArray[i].disabled=true;
         }
-        console.log(inputArray);
-
-        correctAns++;
         addNextButton();
+        clickNext();
       }
       else
       {
         e.target.style.backgroundColor="rgb(206, 51, 51)";
         let ol=e.target.parentElement.parentElement;
         let inputArray=ol.querySelectorAll("ol > li > input");
-        console.log(inputArray);
         for(let i=0;i<4;i++)
         {
             inputArray[i].disabled=true;
         }
+       
         for(let i=0;i<inputArray.length;i++)
         {
             if(inputArray[i].getAttribute("value")==ans)
@@ -68,31 +69,33 @@ container.addEventListener("click",function(e){
             }
         }
         addNextButton();
+        clickNext();
       }
     }
-   
-    clickNext();
   });
 }
 
 let nextBtn;
 function addNextButton()
 {
-     nextBtn=document.createElement("input");
+    removeElement();
+     nextBtn=document.createElement("button");
     nextBtn.innerHTML="Next";
-    nextBtn.type="button";
     nextBtn.value="NEXT";
     nextBtn.classList.add("nextButton");
-    qst.appendChild(nextBtn);
+    questions[currentQuestionNo].appendChild(nextBtn);
 }
-
 
 function clickNext(){
     nextBtn.addEventListener("click",function(){ 
-        nextBtn.disabled=true;
+        currentQuestionNo++;
         visibleQuestion();
-        // addNextButton();
-        // mainFun();
     });
 }
-currentQuestionNo++;
+
+function removeElement(){
+    for(let i=0;i<btn.length;i++)
+    {
+        btn[i].remove();
+    }
+}
