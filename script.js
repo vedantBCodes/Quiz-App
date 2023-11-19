b23f6b81dc1e31f09dd30d919cf012c09d61893b
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-11-19 14:46:36 +0530

    increased height of container

--- script.js
+++ script.js
@@ -139,7 +139,9 @@ function clickViewScoreButton()
 function showScore()
 {
     let div=document.createElement("div");
-    div.innerHTML=`<h>You scored ${correctAns} out of ${questions.length}</h> !`;
+    div.innerHTML=`<h>You scored ${correctAns} out of ${questions.length-1}</h> !`;
+    div.style.textAlign="center";
+    div.style.fontSize="30px";
     div.classList.add("score");
     questions[10].insertAdjacentElement("afterend",div);
 }
